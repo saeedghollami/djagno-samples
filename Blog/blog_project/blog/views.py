e532d59d0fff0726b58559fb4300826d70b77f4f
Saeed Gholami <saeed.ghollami@gmail.com>
2020-02-13 05:05:00 +0330

    Add bootstrap blog template to work on

--- Blog/blog_project/blog/views.py
+++ Blog/blog_project/blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        "title": "Post 1",
+        "author": "someone",
+        "content": "first content",
+        "date": "January 1, 2017",
+    },
+    {
+        "title": "Post 2",
+        "author": "someone",
+        "content": "Second content",
+        "date": "January 1, 2017",
+    },
+    {
+        "title": "Post 3",
+        "author": "someone",
+        "content": "Third content",
+        "date": "January 1, 2017",
+    },
+]
+
+# home page 
+def index(request):
+    return render(request, "blog/index.html", {"posts": posts})
